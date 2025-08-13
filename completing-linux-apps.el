3b85aeb0f7ab042463b2c4a95d1f42d5fca2b3f2
Karl Ragnar Giese <karl@giese.no>
2025-08-13 13:26:50 +0200

    Run programs with dex

--- completing-linux-apps.el
+++ completing-linux-apps.el
@@ -3,10 +3,13 @@
 ;; Copyright (C) 2025, Karl Ragnar Giese
 
 ;; Author: Karl Ragnar Giese <Karl@Giese.no>
-;; Version: 1.0
+;; Version: 1.1
 
 (defvar completing-linux-apps--desktop-entry-cache nil)
 
+(defun completing-linux-apps-run (desktop-entry)
+  "Run desktop entry"
+  (call-process "dex" nil 0 nil desktop-entry))
 
 (defun completing-linux-apps--desktop-entry-files ()
   "Return list of desktop entry files
@@ -18,13 +21,12 @@ These are found by recursively searching xdg data dirs for files ending in .desk
 
 
 (defun completing-linux-apps--build-cache ()
-  "Build the cache, consisting of (name . exec) cons cells"
+  "Build the cache, consisting of (name . entry-file) cons cells"
   (setq completing-linux-apps--desktop-entry-cache 
-	(let* ((files (completing-linux-apps--desktop-entry-files))
-	       (entries (mapcar (lambda (file) (xdg-desktop-read-file file)) files)))
-	  (mapcar (lambda (entry) (cons (gethash "Name" entry)
-				   (gethash "Exec" entry)))
-		  entries))))
+	(let ((files (completing-linux-apps--desktop-entry-files)))
+	  (mapcar (lambda (file) (cons (gethash "Name" (xdg-desktop-read-file file))
+				  file))
+		  files))))
 
 
 (defun completing-linux-apps-clear-cache ()
@@ -39,9 +41,9 @@ These are found by recursively searching xdg data dirs for files ending in .desk
   (unless completing-linux-apps--desktop-entry-cache
     (completing-linux-apps--build-cache))
   (let* ((name (completing-read "Run: " completing-linux-apps--desktop-entry-cache))
-	 (exec (or (cdr (assoc name completing-linux-apps--desktop-entry-cache))
-		   name)))
-    (async-shell-command exec)))
-
+	 (entry (cdr (assoc name completing-linux-apps--desktop-entry-cache))))
+    (if entry
+	(completing-linux-apps-run entry)
+      (async-shell-command name))))
 
 (provide 'completing-linux-apps)
